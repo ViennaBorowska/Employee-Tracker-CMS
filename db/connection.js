ffa5a9029f3e86a859662198666b8f0cb61f2c55
Vienna Borowska <vienna.borowska@hotmail.com>
2022-01-30 17:49:28 +0000

    added .env file to protect MySQL login credentials

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
-
+const env = require('dotenv');
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: 'root',
-    password: 'Manchester2022!',
-    database: 'company_db',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
   console.log('Connected to the company_db databse')
 );
